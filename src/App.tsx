ff7e2968b88ecddf9356550202cd26dbcfae6a42
PairOfJorts <kcouvion@cedarville.edu>
2022-07-13 19:25:39 -0400

    Outlined UI creatively :)

--- src/App.tsx
+++ src/App.tsx
@@ -3,41 +3,18 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [studentID, setStudentID] = useState("");
+  function sendCheckIn() {
+    alert(studentID);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="eventForm">
+      <button className="hideEventForm"></button>
+      <input className="eventID"></input>
+      <button className="createEvent"></button>
+      <select className="selectEvent"></select>
+      <input className="studentID" onChange={(evt) => setStudentID(evt.target.value)}></input>
+      <button className="sendID" type="button" onClick={(() => sendCheckIn())}></button>
     </div>
   )
 }
