f2837c01f98d3d39e8c71d02a6f37dc136905dd2
SpBills <spencebills0@gmail.com>
2022-07-21 00:20:27 +0000

    Functionally MVP complete

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:3001/v1";
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
